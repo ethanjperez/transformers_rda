d208f5c6cd1ccd87818caa98d6f95dd0109dcadb
ethanjperez <ethanperez18@gmail.com>
2021-03-02 22:27:57 -0800

    Adding regression to RDA script

--- rda.py
+++ rda.py
@@ -37,18 +37,22 @@ if __name__ == '__main__':
     parser.add_argument("--data_dir", default='data', type=str, help="The directory in which RDA train/val/test data will be saved."
                         "If your load_data function requires a data path, this directory should also contain the original data you would like to analyze.")
     parser.add_argument("--data_file_ext", default='json', type=str, help="File extension for saved RDA train/val/test data.")
-    parser.add_argument("--num_blocks", default=9, type=int, help="The number of blocks N sent when calculating rda code length (trains N-1 models, since the first block is sent with a uniform prior).")
-    parser.add_argument("--min_block_size", default=64, type=int, help="The minimum number of examples to use in a training block (inclusive).")
-    parser.add_argument("--max_block_size", default=0, type=int, help="The maximum number of examples to use in a training block (exclusive). 0 for all examples.")
+    parser.add_argument("--num_blocks", default=9, type=int, help="The number of blocks N sent when calculating rda code length"
+                        "(trains N-1 models, as the first block is sent with a uniform prior).")
+    parser.add_argument("--min_num_train_samples", default=64, type=int, help="The minimum number of examples to train with.")
+    parser.add_argument("--max_num_train_samples", default=0, type=int, help="The maximum number of examples to use. 0 for all examples.")
     parser.add_argument("--val_frac", default=0.1, type=float, help="The fraction of training examples to split off for validation.")
     parser.add_argument("--seed", default=0, type=int, help="The random seed to use for online coding (e.g., for randomly ordering examples).")
-    parser.add_argument("--label_range", required=True, type=float, help="For classification, the number of possible output classes, used to calculate the codelength for sending the first block using the uniform prior. For regression, the size of the interval over which outputs can range, e.g., 3.5 if the range is [1., 4.5]")
+    parser.add_argument("--label_range", required=True, type=float, help="For classification, the number of possible output classes,"
+                        "used to calculate the codelength for sending the first block using the uniform prior."
+                        "For regression, the size of the interval over which outputs can range, e.g., 3.5 if the range is [1., 4.5]")
+    parser.add_argument("--regression", default=False, action="store_true", help="Whether the task is regression (has continuous-valued targets)")
     args = parser.parse_args()
 
     # Check arguments
     assert args.num_blocks >= 1, '--num_blocks must be >= 1'
-    assert args.min_block_size >= 1, '--min_block_size must be >= 1'
-    assert args.max_block_size >= 0, '--max_block_size must be >= 0'
+    assert args.min_num_train_samples >= 1, '--min_num_train_samples must be >= 1'
+    assert args.max_num_train_samples >= 0, '--max_num_train_samples must be >= 0'
     assert 0 < args.val_frac < 1, '--val_frac must be > 0 and < 1'
     assert args.label_range > 0, '--label_range must be > 0'
 
@@ -58,8 +62,8 @@ if __name__ == '__main__':
     rng.shuffle(dataset)
 
     # Compute data block sizes
-    max_num_train_samples = len(dataset) if args.max_block_size == 0 else min(args.max_block_size, len(dataset))
-    block_start_idxs = np.logspace(np.log10(args.min_block_size), np.log10(max_num_train_samples), args.num_blocks)
+    max_num_train_samples = len(dataset) if args.max_num_train_samples == 0 else min(args.max_num_train_samples, len(dataset))
+    block_start_idxs = np.logspace(np.log10(args.min_num_train_samples), np.log10(max_num_train_samples), args.num_blocks)
     block_start_idxs = np.round(block_start_idxs).astype(int)
     block_start_idxs = np.insert(block_start_idxs, 0, 0, axis=0)
     block_sizes = block_start_idxs[1:] - block_start_idxs[:-1]
@@ -92,8 +96,12 @@ if __name__ == '__main__':
             block_training_args = block_training_args.replace(split.upper() + '_FILE', block_split_filepath)
 
         sys.argv = [train_model.__file__] + block_training_args.split()  # Set command line args for model training
-        test_nll = train_model.main()  # Call main function to train model with above args, to get test NLL on this block
-        nlls.append(test_nll)
+        test_loss = train_model.main()  # Call main function to train model with above args, to get test NLL on this block
+        if args.regression:
+            std_dev = 1.  # Treat all regression predictions as a mean with this std. dev.
+            nlls.append((-test_loss / (2. * (std_dev ** 2))) + np.log(1. / (std_dev * np.sqrt(2 * np.pi))))  # Convert MSE loss to Mean NLL
+        else:
+            nlls.append(test_loss)  # loss for classification is NLL
 
     # Compute MDL
     codelengths = np.array(nlls) / np.log(2)
